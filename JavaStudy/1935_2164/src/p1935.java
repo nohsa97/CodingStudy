04050e26277efcacf8e1b767ecaa31166c52c3c8
nohsa97 <81568105+nohsa97@users.noreply.github.com>
2021-08-13 16:20:53 +0900

    22
    33

---
+++ JavaStudy/1935_2164/src/p1935.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+public class p1935  {
+
+	public static void main(String[]args) throws IOException	{
+		int i;
+		Stack<Integer> IT= new Stack<>();
+		Stack<String> CT= new Stack<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		i=Integer.parseInt(br.readLine());
+		System.out.println(i);
+	}
+}
